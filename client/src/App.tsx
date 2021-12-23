36d122f9311bd609006b72b8459704ae766c5e37
n-wach <nmwachholz@gmail.com>
2021-12-22 18:47:36 -0800

    Add socketio, fix imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import "../../shared";
+import {io, Socket} from "socket.io-client";
+
+const socket: Socket = io();
 
 function App() {
   return (
--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,27 @@
 import * as http from "http";
 import express from "express";
 import path from "path";
+import { Server } from "socket.io";
+import {Game} from "../client/src/shared";
+
+let game: Game;
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 });
 
 app.use("/static", express.static(path.resolve(__dirname, "../client/build/static")))
+
+io.on("connection", (socket) => {
+  console.log("player connected");
+  socket.on('disconnect', () => {
+    console.log('player disconnected');
+  });
+});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
