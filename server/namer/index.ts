8b2b702cd91fb33804c302787d10977f131db461
n-wach <nmwachholz@gmail.com>
2021-12-23 13:02:09 -0800

    Add namer from simultanio

---
+++ server/namer/index.ts
@@ -0,0 +1,25 @@
+import {randomInt} from "crypto";
+import * as fs from "fs";
+import path from "path";
+
+function getLines(file: string): string[] {
+  const relpath = path.resolve(__dirname, file);
+  const buffer = fs.readFileSync(relpath);
+  return buffer.toString().split("\n");
+}
+
+function randomItem<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+const adjectives: string[] = getLines("adjectives.txt");
+const nouns: string[] = getLines("nouns.txt");
+
+export function randomAdjective(): string {
+  return randomItem(adjectives);
+}
+
+export function randomNoun() {
+  return randomItem(nouns);
+}
+
