f4c28e43265f1c3b705495aa15052f4ac5e581b1
Joseph Sullivan <josephsullivan256@gmail.com>
2021-12-23 14:20:45 -0800

    started work on playergrid

--- client/src/PlayingArea.tsx
+++ client/src/PlayingArea.tsx
@@ -1,6 +1,7 @@
 import GameClient from "./GameClient";
-import {Game, GameId} from "./shared";
+import {Game, GameId, Player} from "./shared";
 import React from "react";
+import './PlayingArea.css';
 
 type PlayingAreaProps = {
   client: GameClient;
@@ -20,7 +21,20 @@ export default class PlayingArea extends React.Component<PlayingAreaProps> {
           <code>
             {JSON.stringify(this.props.game, undefined, 1)}
           </code>
+          <PlayerGrid {...this.props}/>
         </div>
     );
   }
+}
+
+function PlayerGrid(props: PlayingAreaProps) {
+  return (<div className="Player-grid">
+    {props.client.get_other_players().map((player) => {
+      return <PlayerCard {...player}/>
+    })}
+  </div>);
+}
+
+function PlayerCard(props: Player) {
+  return (<p style={ {color: props.color}}>■</p>)
 }
